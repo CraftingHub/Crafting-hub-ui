94d49684e3f3a37a7944f314e5cf8422a5a6eb0c
na0495 <saadmrabet008@gmail.com>
2021-09-13 22:41:36 +0100

    add navbar & update stype

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Routing from './routes/routing'
 import { ChakraProvider } from "@chakra-ui/react"
+import Navbar from  './layouts/Navbar'
 
 
 
 function App() {
   return (
      <ChakraProvider>
+       <Navbar/>
        <Routing />
      </ChakraProvider>
   );
