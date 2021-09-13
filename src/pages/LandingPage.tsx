5308876f7d0c17a56761748cd80b163404b36542
na0495 <saadmrabet008@gmail.com>
2021-09-13 22:33:47 +0100

    create Features

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Hero from '../components/home/Hero';
 import Footer from '../components/home/Footer';
 import Review from '../components/home/Review';
+import Features from '../components/home/Features';
 
 
 export default function LandingPage() {
@@ -9,6 +10,7 @@ export default function LandingPage() {
     return (
         <React.Fragment>
            <Hero/>
+           <Features/>
            <Review/>
            <Footer/>
         </React.Fragment>
